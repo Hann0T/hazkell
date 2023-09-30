92edee70d1a81f4592f7426e98e4457f1a91e275
hans <hansolivas1@gmail.com>
2023-09-30 16:45:42 -0500

    first commit.

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*.html"],
+    theme: {
+        extend: {
+            colors: {
+                'dark': '#010B14',
+                'light': '#ffffff',
+                'primary': '#F25D07',
+                'secondary': '#2A87C8'
+            },
+            fontFamily: {
+                'title': ['Montserrat'],
+                'subtitle': ['Lora'],
+                'paragraph': ['Noto Serif'],
+            }
+        },
+    },
+    plugins: [],
+}
+
